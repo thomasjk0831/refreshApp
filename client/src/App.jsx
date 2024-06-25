e0e67bdccf093f948e73c98a118d79d00d6b39f1
Thomas Kim <thomaskim08310@gmail.com>
2024-06-25 13:36:33 -0400

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  HomeLayout,
+  Register,
+  Login,
+  DashboardLayout
+} from './pages'
+// import { HomeLayout } from './pages'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />
+  },
+
+])
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ client/src/pages/Deletejob.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Deletejob = () => {
+  return (
+    <div>Deletejob</div>
+  )
+}
+
+export default Deletejob
