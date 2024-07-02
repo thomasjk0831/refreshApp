0d6be6964a7e3491dc2973182f47b95b90a14a01
Thomas Kim <thomaskim08310@gmail.com>
2024-07-02 12:34:20 -0400

    styled component

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledBtn = styled.button`
+    font-size:1.5rem;
+    background: red;
+    color: white;
+`
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <h1>
+      Landing
+        </h1>
+        <StyledBtn>button</StyledBtn>
+    </div>
   )
 }
 
