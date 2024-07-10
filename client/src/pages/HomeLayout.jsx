fe021d4e816992bc0da4b6d378e8f62acc811a4c
Thomas Kim <thomaskim08310@gmail.com>
2024-07-10 10:52:44 -0400

    landing

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom'
 const HomeLayout = () => {
   return (
     <div>
-      <h1>HomeLayout</h1>
       <Outlet />
     </div>
   )
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 
-const StyledBtn = styled.button`
-    font-size:1.5rem;
-    background: ${props => props.bg === "red"? "red": "blue"};
-`
 
 const Landing = () => {
   return (
-    <div>
-      <h1>
-      Landing
-        </h1>
-        <StyledBtn bg="red">button</StyledBtn>
-    </div>
+  <Wrapper>
+      <nav>
+          <img src={logo} alt ='jobify' className='logo' />      
+      </nav>  
+      <div className='container page'>
+          <div className='info'>
+              <h1>
+                job 
+                <span>tracking</span>
+                app
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, nostrum adipisci. Accusamus maiores quasi dolorum possimus qui dolore, consequuntur culpa praesentium veritatis cupiditate natus fugiat unde eos obcaecati velit at!
+              </p>
+              <Link to="/register" className='btn register-link'>
+               Register
+              </Link>
+              <Link to='/login' className="btn">
+              Login 
+              </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+  </Wrapper>
   )
 }
 
