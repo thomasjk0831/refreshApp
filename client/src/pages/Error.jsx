5df0fdce582ca114a1ec391acb1adb1e0eaa4196
Thomas Kim <thomaskim08310@gmail.com>
2024-07-16 15:05:42 -0400

    error

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { useRouteError, Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/ErrorPage'
+import img from '../assets/images/not-found.svg'
 
 const Error = () => {
   const error = useRouteError()
   console.log(error)
+  if(error.status === 404){
+      return <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>page not found</h3>
+    <Link to="/dashboard">home</Link>
+
+        </div>
+      </Wrapper>
+  }
+
   return (
     <div>
     <h1>Error</h1>
