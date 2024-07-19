c490e1e0c268ce0573b3fe11a1117dff9ffe5a7d
Thomas Kim <thomaskim08310@gmail.com>
2024-07-19 07:28:59 -0400

    login

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import Logo  from '../components/Logo'
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login</Link>
-      </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor='name' className='form-label'>
+            name
+            </label>
+            <input 
+              type='text' 
+              id='name' 
+              name='name' 
+              className='form-input' 
+              defaultValue="default"
+              required
+            />
+        </div>
+        <button type='submit' className='btn btn-block'>submit</button>
+        <p>
+          Already a member?
+        <Link to ='/login' className='member-btn'>Login</Link>
+        </p>
+      </form>
+      </Wrapper>
   )
 }
 
