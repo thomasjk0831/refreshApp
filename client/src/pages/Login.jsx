14d5babc53a02dce6f668d11ae3fdcd42257e757
Thomas Kim <thomaskim08310@gmail.com>
2024-07-23 08:31:55 -0400

    login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow, Logo } from '../components'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/register">Register</Link>
-      </div>
+    <Wrapper>
+     <form className="form">
+      <Logo />
+      <h4>login</h4>
+      <FormRow type="email" name="email" defaultValue="thomas@gmail.com" />
+      <FormRow type="password" name="password" defaultValue="secret123" />
+      <button type='submit' className='btn btn-block'>
+      submit
+      </button>
+      <button type='submit' className='btn btn-block'>
+      explore the app
+      </button>
+      <p>
+        Not a member yet?
+          <Link to='/register' className='member-btn'>
+          Register
+          </Link>
+        `
+      </p>
+     </form>
+      </Wrapper>
   )
 }
 
