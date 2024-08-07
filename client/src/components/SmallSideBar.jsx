ea4ec0dc1157b906c7c1e3538010603f52371476
Thomas Kim <thomaskim08310@gmail.com>
2024-08-07 11:46:19 -0400

    sidebar

--- client/src/components/SmallSideBar.jsx
+++ client/src/components/SmallSideBar.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
 import { useDashboardContext } from '../pages/DashboardLayout'
+import { FaTimes } from 'react-icons/fa'
+import Logo from './Logo'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
 
 const SmallSidebar = () => {
   const data = useDashboardContext()
   console.log(data)
   return (
-    <Wrapper>SmallSidebar</Wrapper>
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type ='button' className="close-btn">
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map(({ text, path, icon})=> {
+              return (
+              <NavLink to ={path} key={text} className="nav-link">
+                <span className="icon">
+                  {icon}
+                </span>
+                {text}
+              </NavLink>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
