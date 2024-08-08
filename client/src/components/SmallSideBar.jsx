77ffc3fe0bbbc90e489aa00a6488b36cd6baaeca
Thomas Kim <thomaskim08310@gmail.com>
2024-08-08 09:59:30 -0400

    sidebar

--- client/src/components/SmallSideBar.jsx
+++ client/src/components/SmallSideBar.jsx
@@ -8,28 +8,26 @@ import { NavLink } from 'react-router-dom'
 
 const SmallSidebar = () => {
   const data = useDashboardContext()
-  console.log(data)
+  const { showSidebar, toggleSidebar } = useDashboardContext()
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div className={showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"} >
         <div className="content">
-          <button type ='button' className="close-btn">
+          <button type ='button' className="close-btn" onClick={toggleSidebar}>
             <FaTimes />
           </button>
           <header>
             <Logo />
           </header>
           <div className="nav-links">
-            {links.map(({ text, path, icon})=> {
-              return (
-              <NavLink to ={path} key={text} className="nav-link">
+            {links.map(({ text, path, icon})=> 
+              <NavLink to ={path} key={text} className="nav-link" onClick={toggleSidebar} end>
                 <span className="icon">
                   {icon}
                 </span>
                 {text}
               </NavLink>
-              )
-            })}
+            )}
           </div>
         </div>
       </div>
